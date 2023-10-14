e9cca353b5f66a45a77ad636c38c8cb4da8d2370
Nikola Vucenic <nikola.vucenic@vega.local>
2023-10-14 15:10:25 +0200

    Add on click

--- lib/ui/benefits_page/view/benefits_view.dart
+++ lib/ui/benefits_page/view/benefits_view.dart
@@ -8,14 +8,15 @@ class BenefitsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFF6F5FA),
-      appBar: AppBar(backgroundColor: Colors.orange),
-      body:  Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15,),
-          child: Column(
-            children: [
-               Padding(
+        backgroundColor: const Color(0xFFF6F5FA),
+        appBar: AppBar(backgroundColor: Colors.orange),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 15,
+            ),
+            child: Column(children: [
+              Padding(
                 padding: const EdgeInsets.only(top: 10, bottom: 15),
                 child: Text(
                   'Besplatne ili povlašćene usluge',
@@ -26,17 +27,29 @@ class BenefitsView extends StatelessWidget {
                   ),
                 ),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Text('Filtriraj',
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => FilterView()),
+                  );
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Filtriraj',
                       style: TextStyle(
                         fontSize: 14,
                         color: AppColors.royalBlue,
-                      )),
-                  const SizedBox(width: 8),
-                  const Image(image: AssetImage('assets/images/icons/filter_icon.png')),
-                ],
+                      ),
+                    ),
+                    const SizedBox(width: 8),
+                    const Image(
+                        image:
+                            AssetImage('assets/images/icons/filter_icon.png')),
+                  ],
+                ),
               ),
               SizedBox(
                 height: MediaQuery.of(context).size.height - 214,
@@ -45,12 +58,10 @@ class BenefitsView extends StatelessWidget {
                     itemCount: 3,
                     itemBuilder: (BuildContext context, int index) {
                       return const BenefitCard();
-                    }
-                ),
+                    }),
               )
-    ]
-        ),
-      ),
-    ));
+            ]),
+          ),
+        ));
   }
 }
