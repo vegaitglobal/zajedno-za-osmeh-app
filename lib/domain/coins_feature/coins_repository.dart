0f539f1c09a924c3a9176944f2b61718a97c7ddf
d-cetkovic <96722044+d-cetkovic@users.noreply.github.com>
2023-10-14 16:45:37 +0200

    refactor(Coins) remove legacy feature for Coin model - not used

--- lib/domain/coins_feature/coins_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'model/coin_model.dart';
-
-abstract class CoinsRepository {
-  Future<bool> fetchCoins(int count, int page);
-
-  Stream<List<CoinModel>> get coinList;
-
-  Future<List<CoinModel>> getCoinsDatabase();
-}
