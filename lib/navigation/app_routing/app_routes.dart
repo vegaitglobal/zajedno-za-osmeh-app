b918437857d64d3f900d911af03c0bdf8b830a7b
M1K1B <miki.balabanovic111@gmail.com>
2023-10-15 16:11:17 +0200

    feat: added login, signup and upload medical file widgets

--- lib/navigation/app_routing/app_routes.dart
+++ lib/navigation/app_routing/app_routes.dart
@@ -1,8 +1,19 @@
-enum AppRoutes { home, benefits, benefitDetail, aboutUs, donate, qrVerification, qrScanner, qrResult }
+enum AppRoutes {
+  home,
+  authentification,
+  benefits,
+  benefitDetail,
+  aboutUs,
+  donate,
+  qrVerification,
+  qrScanner,
+  qrResult,
+}
 
 extension AppRoutesExtension on AppRoutes {
   String path() => switch (this) {
         AppRoutes.home => '/home',
+        AppRoutes.authentification => '/authentification',
         AppRoutes.benefits => '/benefits',
         AppRoutes.benefitDetail => '/benefitDetail',
         AppRoutes.aboutUs => '/aboutUs',
