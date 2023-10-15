c1f56989a6463e92aae5fdd178a203f8d0047404
igorMales98 <igor_malesevic@yahoo.com>
2023-10-15 13:33:24 +0200

    Adjust model

--- lib/data/filter_feature/repository/filter_repository_impl.dart
+++ lib/data/filter_feature/repository/filter_repository_impl.dart
@@ -1,21 +1,22 @@
-import 'package:flutter/material.dart';
 import 'package:gu_mobile/data/core/supabase/supabase_client.dart';
+
 import '../../../ui/filter_page/model/FilterUiModel.dart';
-import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class FilterRepository {
+  FilterRepository();
 
   Future<List<FilterUiModel>> getFiltersCall() async {
     final response = await supabaseClient.from('Category').select();
     List<FilterUiModel> filters = response
-      .map<FilterUiModel>(
-        (category) => FilterUiModel(
-          id: category['id'], 
-          createdAt: category['created_at'],
-          name: category['name'],
-          icon: category['icon_name'],
-        ),
-      ).toList();
-      return filters;
+        .map<FilterUiModel>(
+          (category) => FilterUiModel(
+            id: category['id'],
+            createdAt: category['created_at'],
+            name: category['name'],
+            icon: category['icon_name'],
+          ),
+        )
+        .toList();
+    return filters;
   }
 }
--- lib/ui/filter_page/model/FilterUiModel.dart
+++ lib/ui/filter_page/model/FilterUiModel.dart
@@ -3,7 +3,7 @@ import 'package:json_annotation/json_annotation.dart';
 @JsonSerializable()
 class FilterUiModel {
   int id;
-  DateTime createdAt;
+  String createdAt;
   String name;
   String icon;
 
