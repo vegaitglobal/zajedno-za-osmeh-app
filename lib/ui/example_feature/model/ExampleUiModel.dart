5e6112aa0bee96cd3a4264c293da1c3ddcbb5da0
d-cetkovic <96722044+d-cetkovic@users.noreply.github.com>
2023-10-23 18:38:36 +0200

    feature(registration) remove example feature

--- lib/ui/example_feature/model/ExampleUiModel.dart
+++
@@ -1,9 +0,0 @@
-
-import 'package:json_annotation/json_annotation.dart';
-
-@JsonSerializable()
-class ExampleUiModel {
-  int numberOfItems;
-  bool isLoadingFailed;
-  ExampleUiModel({required this.numberOfItems, required this.isLoadingFailed});
-}
