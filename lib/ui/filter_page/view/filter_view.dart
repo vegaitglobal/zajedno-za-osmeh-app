97c208b67f2faf375d3eebc497958d62158df3ad
Tomislav Petkovic <t.petkovic@vegait.rs>
2023-10-23 16:09:27 +0200

    Add button and closing icon.

--- lib/ui/filter_page/view/filter_view.dart
+++ lib/ui/filter_page/view/filter_view.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:gu_mobile/navigation/app_routing/app_routes.dart';
+import 'package:gu_mobile/resources/my_colors.dart';
 import 'package:gu_mobile/ui/benefits_feature/bloc/benefits_bloc.dart';
 import 'package:gu_mobile/ui/filter_page/view/categories_wrap.dart';
 import 'package:gu_mobile/ui/filter_page/view/clients_dropdown.dart';
@@ -10,35 +13,115 @@ class FilterView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            children: [
-              const Text('Filteri'),
-              BlocBuilder<BenefitsBloc, BenefitsState>(
-                builder: (context, state) {
-                  return switch (state) {
-                    BenefitsLoadingState() => const Center(
-                        child: CircularProgressIndicator(),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    context.go(AppRoutes.benefits.path());
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      const Text(
+                        'Zatvori',
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
                       ),
-                    BenefitsInitial() => const Placeholder(),
-                    BenefitsSuccessState() => Column(
-                        children: [
-                          CategoriesWrap(
-                            categories: state.categories,
-                            selectedCategories: state.selectedCategories,
-                          ),
-                          ClientsDropdown(
-                            selectedCity: state.selectedCity,
-                            cities: state.cities,
-                          ),
-                        ],
+                      const SizedBox(
+                        width: 10,
                       ),
-                    BenefitsFailState() => const Placeholder()
-                  };
-                },
-              ),
-            ],
+                      Image.asset("assets/images/icons/cross.png")
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 40),
+                const Text(
+                  'Filteri',
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                const SizedBox(height: 25),
+                BlocBuilder<BenefitsBloc, BenefitsState>(
+                  builder: (context, state) {
+                    return switch (state) {
+                      BenefitsLoadingState() => const Center(
+                          child: CircularProgressIndicator(),
+                        ),
+                      BenefitsInitial() => const Placeholder(),
+                      BenefitsSuccessState() => Column(
+                          children: [
+                            const SizedBox(
+                              width: double.infinity,
+                              height: 30,
+                              child: Text(
+                                'Kategorije',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 18,
+                                ),
+                                textAlign: TextAlign.start,
+                              ),
+                            ),
+                            CategoriesWrap(
+                              categories: state.categories,
+                              selectedCategories: state.selectedCategories,
+                            ),
+                            const SizedBox(height: 20),
+                            const SizedBox(
+                              width: double.infinity,
+                              height: 40,
+                              child: Text(
+                                'Lokacija',
+                                style: TextStyle(
+                                    color: Colors.black, fontSize: 18),
+                                textAlign: TextAlign.start,
+                              ),
+                            ),
+                            ClientsDropdown(
+                              selectedCity: state.selectedCity,
+                              cities: state.cities,
+                            ),
+                            const SizedBox(height: 40),
+                            GestureDetector(
+                                onTap: () {},
+                                child: Container(
+                                  width: double.infinity,
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 16, vertical: 10),
+                                  decoration: BoxDecoration(
+                                    borderRadius: const BorderRadius.all(
+                                      Radius.circular(8),
+                                    ),
+                                    color: true
+                                        ? AppColors.buttonEnabled
+                                        : AppColors.buttonDisabled,
+                                  ),
+                                  child: const Text(
+                                    'Primeni',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w700,
+                                    ),
+                                  ),
+                                )),
+                          ],
+                        ),
+                      BenefitsFailState() => const Placeholder()
+                    };
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
