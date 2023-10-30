04b7597f1fb0acc2ad15bf9b4bc2e32d1e57a281
Tomislav Petkovic <t.petkovic@vegait.rs>
2023-10-30 12:15:31 +0100

    Remove filter chip widget with custom created

--- lib/ui/filter_page/view/categories_wrap.dart
+++ lib/ui/filter_page/view/categories_wrap.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gu_mobile/resources/my_colors.dart';
 import 'package:gu_mobile/ui/benefits_feature/bloc/benefits_bloc.dart';
 import 'package:gu_mobile/ui/filter_page/model/FilterUiModel.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
@@ -20,36 +21,49 @@ class CategoriesWrap extends StatelessWidget {
   Widget build(BuildContext context) {
     return Wrap(spacing: 8.0, children: [
       ...categories.map((category) {
+        final isSelected =
+            selectedCategories.map((e) => e.id).contains(category.id);
         return Padding(
-          padding: const EdgeInsets.all(2),
-          child: FilterChip(
-            label: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Icon(MdiIcons.fromString(category.icon)),
-                const SizedBox(width: 4.0),
-                Text(
-                  category.name,
-                  style: const TextStyle(fontSize: 16),
-                ),
-              ],
-            ),
-            shape: RoundedRectangleBorder(
-              side: const BorderSide(color: Colors.black, width: 1.0),
-              borderRadius: BorderRadius.circular(20.0),
-            ),
-            backgroundColor: Colors.transparent,
-            selected: selectedCategories.map((e) => e.id).contains(category.id),
-            onSelected: (bool value) {
+          padding: const EdgeInsets.symmetric(horizontal: 2.5, vertical: 5),
+          child: InkWell(
+            onTap: () {
               onPress();
-              if (value) {
-                context.read<BenefitsBloc>().add(AddCategoryFilter(category));
-              } else {
+              if (isSelected) {
                 context
                     .read<BenefitsBloc>()
                     .add(RemoveCategoryFilter(category));
+              } else {
+                context.read<BenefitsBloc>().add(AddCategoryFilter(category));
               }
             },
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                border: Border.all(
+                  color: const Color.fromARGB(133, 0, 0, 0),
+                  width: 1,
+                ),
+                color: isSelected ? AppColors.royalBlue : Colors.transparent,
+              ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(
+                    MdiIcons.fromString(category.icon),
+                    color: AppColors.primaryOrange,
+                  ),
+                  const SizedBox(width: 7),
+                  Text(
+                    category.name,
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: isSelected ? Colors.white : Colors.black,
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
         );
       }).toList()
