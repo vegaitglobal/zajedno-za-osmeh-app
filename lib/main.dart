7029c4d4c3cca54ca2cc416915ee0a6c9680a973
Tomislav Petkovic <t.petkovic@vegait.rs>
2023-11-14 11:54:16 +0100

    Use dotenv to hide supabase key and supabase url

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:gu_mobile/di/injector.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await initDependencyInjection();
   await initializeSupabase();
   runApp(const Application());
 }
 
+String _supabaseUrl = dotenv.env['SUPABASE_URL'] ?? "";
+String _supabaseKey = dotenv.env['SUPABASE_API_KEY'] ?? "";
+
 Future<void> initializeSupabase() async => await Supabase.initialize(
       url: _supabaseUrl,
       anonKey: _supabaseKey,
     );
-
-const _supabaseKey =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImppdXN0dXNqc212Znl3ZXVtYWlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTcxOTM0MzcsImV4cCI6MjAxMjc2OTQzN30.jcIbuM4-2ETToiBiTjQPB06NaQt2LPrq6iywpCMF_YI';
-const _supabaseUrl = 'https://jiustusjsmvfyweumaip.supabase.co';
